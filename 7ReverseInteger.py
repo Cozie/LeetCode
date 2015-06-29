d8ce2b6f6e1288ce71d7bf1c1c38f6387f108d50
Cozie <Cozie@users.noreply.github.com>
2015-06-29 02:35:56 -0500

    Update 7ReverseInteger.py

--- 7ReverseInteger.py
+++ 7ReverseInteger.py
@@ -5,12 +5,13 @@ class Solution:
         s=1
         
         if x<0:
-            x=-x
-            s=-1
+            x=-x     # convert negative into positive integer
+            s=-1     # mark as negative
 
         ans=int(str(x)[::-1])
 
-        if ans< pow(2,31):
+        if ans< pow(2,31):    # check if overflows
             return ans*s
         else:
             return 0
+
